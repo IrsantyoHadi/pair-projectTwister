7d773065364b2d1218199f049ba7773d740b943d
Irsantyo Hadi <irsantyohadi@gmail.com>
2019-05-23 14:33:37 +0700

    first commit generate table and add constraint

---
+++ models/tweet.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Tweet = sequelize.define('Tweet', {
+    Post: DataTypes.STRING,
+    UserId: DataTypes.INTEGER
+  }, {});
+  Tweet.associate = function(models) {
+    // associations can be defined here
+    Tweet.hasMany(models.TweetTag)
+    Tweet.belongsTo(models.Retweet)
+    Tweet.belongsTo(models.User)
+  };
+  return Tweet;
+};
